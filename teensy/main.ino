cbebbe0102246993348751c67cc32640d63feacc
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-09-07 21:56:50 -0700

    edited main

--- teensy/main.ino
+++ teensy/main.ino
@@ -1,9 +1,15 @@
+int i = 0;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("Teensy Counting Sketch");
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  i++;
+  Serial.println(i);
+  delay(1000);
 }
